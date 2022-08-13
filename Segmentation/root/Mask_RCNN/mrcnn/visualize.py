d07dded6d86f49c233e5ffaad58632f970d31298
DDDog-WANG <superalpacaaa@gmail.com>
2022-08-13 10:58:43 +0900

    20220813 run the lastest segmentation, add results and qsub220808

--- Segmentation/root/Mask_RCNN/mrcnn/visualize.py
+++ Segmentation/root/Mask_RCNN/mrcnn/visualize.py
@@ -108,12 +108,15 @@ def display_instances_masks(images, results, side):
         plt.imshow(masked_image.astype(np.uint8))
     plt.show()
     
-def display_instances_grid(images, results, side, show_mask=True, show_bbox=True, colors=None, captions=None):
+def display_instances_grid(images, results, side, use_masks=None, show_mask=True, show_bbox=True, colors=None, captions=None):
     fig,ax = plt.subplots(5,5,figsize=(side,side))
     for num in tqdm(range(25)):
         r = results[num]
         image = images[num]
-        boxes,masks,class_ids,scores,class_names = r['rois'],r['masks'],r['class_ids'],r['scores'],["BG","nucleus"]
+        if use_masks == None:
+            boxes,masks,class_ids,scores,class_names = r['rois'],r['masks'],r['class_ids'],r['scores'],["BG","nucleus"]
+        else:
+            boxes,masks,class_ids,scores,class_names = r['rois'],use_masks,r['class_ids'],r['scores'],["BG","nucleus"]
 #         elif use=="pytorch":
 #             boxes,masks,class_ids,scores,class_names = r['boxes'],r['masks'],r['labels'],r['scores'],["BG","nucleus"]
     
@@ -254,6 +257,79 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
         plt.show()
+
+
+def display_instances_save(image, boxes, masks, class_ids, class_names, savename,
+                      scores=None, title="", 
+                      figsize=(10, 10), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None):
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+    
+    fig, ax = plt.subplots(1, figsize=figsize)
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    fig.savefig(savename,bbox_inches = 'tight')
+
 
 
 def display_differences(image,
